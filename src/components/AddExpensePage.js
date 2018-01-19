a8dee5c5da3776944e14f243fe6196cf0b09366c
Facundo <fpezzola@MacBook-Air-de-Facundo.local>
2018-01-18 22:49:45 -0300

    refactor expense list

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -6,8 +6,9 @@ import { addExpense } from '../actions/expenses'
 
 export class AddExpensePage extends React.Component { 
     onSubmit = (expense) => {
+        console.log(expense);
         this.props.addExpense(expense);
-        this.props.history.push("/")
+        this.props.history.push("/");
     }
 
     render(){
